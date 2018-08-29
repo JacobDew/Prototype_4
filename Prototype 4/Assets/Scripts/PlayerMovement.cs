95ba471f1494a09bf0019fc77edd8bf49093b0f5
JacobDew <32725817+JacobDew@users.noreply.github.com>
2018-08-29 16:18:46 +1200

    Added Buggy Player movement (Will NOT use in final build)

---
+++ Prototype 4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Plane playerPlane;
+    public Transform Player;
+    public Ray ray;
+    public float playerSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPlane = new Plane(Vector3.up, transform.position);
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist;
+
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = -ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            transform.rotation = targetRotation;
+
+            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed, Space.World);
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed, Space.World);
+        }
+
+    }
+}
