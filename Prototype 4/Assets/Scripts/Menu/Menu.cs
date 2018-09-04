b6b14ffab3588d08c6543500f13bc5ee913976d8
JacobDew <jacob.dewse@gmail.com>
2018-09-04 23:15:08 +1200

    Added enemy spawner + Ai NEED TO FIX AI ETC
    Added enemy spanner
    AI
    finished Audio Manager 
    functionality of main menu

--- Prototype 4/Assets/Scripts/Menu/Menu.cs
+++ Prototype 4/Assets/Scripts/Menu/Menu.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Text m_VolumeText;
+    public float m_fVolumeLevel;
 
-	
-	// Update is called once per frame
-	void Update()
+    public GameObject slider;
+
+    private void Awake()
     {
-        
+        m_fVolumeLevel = slider.GetComponent<Slider>().value;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = slider.GetComponent<Slider>().value;
+
+        m_fVolumeLevel = slider.GetComponent<Slider>().value;
+        m_VolumeText.text = ((int)(slider.GetComponent<Slider>().value * 100.0f)).ToString();
     }
 
     public void NextScene()
--- Prototype 4/Assets/Scripts/Projectile/ProjectileScript.cs
+++ Prototype 4/Assets/Scripts/Projectile/ProjectileScript.cs
@@ -6,23 +6,33 @@ public class ProjectileScript : MonoBehaviour
 {
     private Vector3 m_vDirection = new Vector3(0.0f, 0.0f, 0.0f);
 
+    private float Speed;
+
 	// Use this for initialization
-	void Start()
+	void Awake()
     {
-		
-	}
+        //Speed of Laser Bullet
+        Speed = 20f;
+
+    }
 	
 	// Update is called once per frame
 	void Update()
     {
 		if (0.0f != m_vDirection.x || 0.0f != m_vDirection.z)
         {
-            this.transform.position += m_vDirection * Time.deltaTime;
+            this.transform.position += m_vDirection * Time.deltaTime * Speed;
         }
 	}
 
     public void SetDirection(Vector3 _Direction)
     {
         m_vDirection = _Direction;
+    }
+
+    //When Projectile goes offscreen it deletes itself
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
